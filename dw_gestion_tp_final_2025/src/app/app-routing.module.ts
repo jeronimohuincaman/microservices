def5205b76a78799faaa92796f989966ee0d833f
Jeropa2 <huincamanjeronimo.dev@gmail.com>
2025-09-09 20:50:25 -0300

    `Feat: Init source` | Creo proyecto source - Angular base con ngmodule - Pantalla inicial. - Tailwind

---
+++ dw_gestion_tp_final_2025/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './modules/home/components/home.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: '**', redirectTo: '' } // ruta wildcard
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ dw_gestion_tp_final_2025/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'dw_gestion_tp_final_2025';
+}
