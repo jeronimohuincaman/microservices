9f749b89905aeb8225c99092ca2a2faa8bc5c596
Jeropa2 <huincamanjeronimo.dev@gmail.com>
2025-09-09 21:08:41 -0300

    tailwind configuration

--- dw_gestion_tp_final_2025/tailwind.config.js
+++ dw_gestion_tp_final_2025/tailwind.config.js
@@ -1,10 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,ts}",
+    "./src/**/*.{html,ts}"  // Escanea todos los templates
   ],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1E40AF",  // Azul base
+        secondary: "#9333EA", // Violeta
+        accent: "#F59E0B",   // Amarillo
+        success: "#10B981",  // Verde
+        warning: "#FBBF24",  // Amarillo claro
+        danger: "#EF4444",   // Rojo
+      },
+    },
+  },
   plugins: [],
-
 }
-
