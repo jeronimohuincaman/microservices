1a2c78da5e255fe680d4cda4f8f5914503b5b209
Jeropa2 <huincamanjeronimo.dev@gmail.com>
2025-09-22 12:27:10 -0300

    `Feat: Facturas` | Add estructura de archivos.

--- dw_gestion_tp_final_2025/src/app/app-routing.module.ts
+++ dw_gestion_tp_final_2025/src/app/app-routing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from './layout/layout.component';
 import { HomeComponent } from './modules/home/components/home.component';
+import { FacturasComponent } from './modules/facturas/components/facturas.component';
 
 const routes: Routes = [
   {
     path: '',
     component: LayoutComponent,
     children: [
-      { path: '', component: HomeComponent }
+      { path: '', component: HomeComponent },
+      { path: 'facturas', component: FacturasComponent }
     ]
   },
   { path: '**', redirectTo: '' } // ruta wildcard
---
+++ dw_gestion_tp_final_2025/src/app/modules/facturas/facturas.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FacturasComponent } from './components/facturas.component';
+
+
+
+@NgModule({
+  declarations: [
+    FacturasComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class FacturasModule { }
