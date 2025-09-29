e11a8be3095be8f874d2939a78399055d939cf71
Jeropa2 <huincamanjeronimo.dev@gmail.com>
2025-09-28 23:55:12 -0300

    add angular material + fix ruta de facturas

--- dw_gestion_tp_final_2025/src/app/app-routing.module.ts
+++ dw_gestion_tp_final_2025/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { LayoutComponent } from './layout/layout.component';
 import { HomeComponent } from './modules/home/components/home.component';
 import { FacturasComponent } from './modules/facturas/components/facturas.component';
 import { UsuariosComponent } from './modules/usuarios/components/usuarios.component';
+import { ProductosComponent } from './modules/productos/components/productos.component';
 
 const routes: Routes = [
   {
@@ -13,7 +14,7 @@ const routes: Routes = [
       { path: '', component: HomeComponent },
       { path: 'usuarios', component: UsuariosComponent },
       { path: 'facturas', component: FacturasComponent },
-      { path: 'productos', component: FacturasComponent }
+      { path: 'productos', component: ProductosComponent }
     ]
   },
   { path: '**', redirectTo: '' } // ruta wildcard
