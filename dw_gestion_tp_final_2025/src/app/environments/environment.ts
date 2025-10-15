a30c3bc0d55fd12e9775d4ea03f3aab3e92ae8b1
DiLucaLucas <lucasmartindiluca@gmail.com>
2025-10-14 21:23:02 -0300

    `Feat: Environment` | Add ennvironment file.

---
+++ dw_gestion_tp_final_2025/src/app/environments/environment.ts
@@ -0,0 +1,7 @@
+const $BASE_URL = "http://localhost:3000/api/v1";
+
+export const environment = {
+    usuarios: `${$BASE_URL}/users`,
+    facturas: `${$BASE_URL}/facturas`,
+    productos: `${$BASE_URL}/products`
+}
